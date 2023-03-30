98ddb2d7497a28381f7344cc1c042c3eb37817ea
leeinxuan <x14725836961@gmail.com>
2023-03-30 10:49:07 +0800

    css grid排版

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+
+
+function Home(){
+    return(
+        <div className="container mainLayout">
+            <div className="layoutHeader"><h1>Header</h1></div>
+            <div className="layoutContent"><h1>content</h1></div>
+            <div className="layoutFooter"><h1>footer</h1></div>
+        </div>
+    )
+}
+
+export default Home;
