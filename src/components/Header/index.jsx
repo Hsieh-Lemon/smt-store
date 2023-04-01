907fa9b8abd6782d4863a048d756545e18fb0eb4
leeinxuan <x14725836961@gmail.com>
2023-04-01 12:21:03 +0800

    add Footer

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,8 @@ import styles from "./header.module.css";
 
 export default function Header(){
     return(
-
+        <div className={styles.header}>
+            <img className={styles.logo} src="/images/LOGO2.png" alt="logo" />
+        </div>
     )
 }
