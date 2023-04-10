de5349c429ede7561fbbaf7a53aa74214e767291
HYC <firstoneme@gmail.com git add .git pushgit config --listgit config --global user.email firstoneme@gmail.com>
2023-04-10 23:36:22 +0800

    final-rr

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { configureStore } from '@reduxjs/toolkit';
 //import { persistReducer, persistStore } from 'redux-persist';
 //import thunk from 'redux-thunk';
 import cartReducer from './cartSlice';
-import colorReducer from './colorSLice';
+import colorReducer from './colorSlice';
 
 // Data Persist Config
 // const persistConfig = {
